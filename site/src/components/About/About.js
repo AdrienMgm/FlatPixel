25290cff3faddfdeab079b3d60afd7e15ee73dce
maxime blondeau <maxime.blondeau@firstborn.com>
2019-03-13 00:20:13 -0400

    add nav to about

--- site/src/components/About/About.js
+++ site/src/components/About/About.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './style.scss';
+import Nav from '../Nav/Nav';
 
 export default () => {
 	return (
 		<div className="about">
+			<Nav/>
 			<h2>Hello World !</h2>
 			<p>@FlatPixel is a small game development team that packs a big punch! Along with our squad of international collaborators, we strive to deliver #juicy, robust video games to be enjoyed without moderation.</p>
 		</div>
